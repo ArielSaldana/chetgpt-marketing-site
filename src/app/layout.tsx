c988ca2d71a9a10144915e3474100900b55d2ae6
Ariel Saldana <ariel@unreal.io>
2024-05-03 13:32:40 -0400

    Feature Viewport Fix (#3)
    * Feature Viewport Fix
    
    * Merge declarations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -8,6 +8,13 @@ export const metadata: Metadata = {
   title: "ChetGPT",
   description: "ChatGPT is a free-to-use MEME AI system. Use it for engaging conversations, gain insights, automate tasks, and witness the future of memes through AI, all in one place.",
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export default function RootLayout({
   children,
